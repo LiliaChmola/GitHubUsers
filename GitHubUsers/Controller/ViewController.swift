712f59a0ecb1035db0fc73444a5b0f48c498aec9
Lilia Chmola <chmola.lilia@gmail.com>
2019-07-14 17:32:33 +0300

    - Update UI

--- GitHubUsers/Controller/ViewController.swift
+++ GitHubUsers/Controller/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var collectionViewHeightLayoutConstraint: NSLayoutConstraint!
+    @IBOutlet weak var findASchoolButton: UIButton! {
+        didSet {
+            findASchoolButton.setStyle()
+        }
+    }
+    @IBOutlet weak var findInstructorButton: UIButton! {
+        didSet {
+            findInstructorButton.setStyle()
+        }
+    }
     private let networkManager = NetworkManager()
     private let since = 0
     private var users = [User]()
@@ -32,7 +42,7 @@ class ViewController: UIViewController {
 
 // MARK: - UICollectionViewDataSource
 extension ViewController: UICollectionViewDataSource {
-   
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return users.count
     }
@@ -46,8 +56,18 @@ extension ViewController: UICollectionViewDataSource {
         cell.typeLabel.text = String(user.type)
         cell.loginLabel.text = String(user.login)
         cell.imageView.sd_setImage(with: URL(string: user.avatarUrl), placeholderImage: UIImage(named: "placeholder"))
-
+        
         return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.item == (users.count-1) {
+            print("Last cell")
+        }
     }
 }
 
@@ -56,5 +76,17 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let screenSize = UIScreen.main.bounds.size
         return CGSize.init(width: screenSize.width * 0.4, height: screenSize.height * 0.4)
+    }
+}
+
+// MARK: - UIButton extension
+fileprivate extension UIButton {
+    func setStyle() {
+        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowRadius = 8
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = 4.0
     }
 }
